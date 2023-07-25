282a6a1d6e1bb83570fda9b4a1796ea8a5c3834e
Namnpse <nam444479@gmail.com>
2023-07-25 09:07:39 +0700

    support screen sizes using separate layouts for different types of screens

---
+++ app/src/main/java/com/namnp/screen_size/SupportScreenSizeByDensityActivity.kt
@@ -0,0 +1,11 @@
+package com.namnp.screen_size
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SupportScreenSizeByDensityActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_support_screen_size_by_density)
+    }
+}
